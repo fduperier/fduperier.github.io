4fb723221e12c5b2ac57ecf16eb5b43be7da949a
Frédéric Dupérier <fduperier@gmail.com>
2014-01-30 20:22:58 +0100

    Profile picture + GA Events for personal website

--- assets/javascripts/analytics.js
+++ assets/javascripts/analytics.js
@@ -4,20 +4,27 @@
 var ga = ga || [];
 
 var sendGAEvent = function(e) {
-  ga('send', 'event', 'Social Networks', 'click', e.data.label);
+  ga('send', 'event', e.data.category, 'click', e.data.label);
 };
 
-$('a.btn').each(function() {
+$('#social-networks-links a.btn').each(function() {
   /** Personal **/
-  $(this).has('.fa-facebook').on('click', { label: 'Facebook' }, sendGAEvent);
-  $(this).has('.fa-instagram').on('click', { label: 'Instagram' }, sendGAEvent);
-  $(this).has('.fa-vimeo').on('click', { label: 'Vimeo' }, sendGAEvent);
-  $(this).has('.fa-youtube').on('click', { label: 'Youtube' }, sendGAEvent);
+  $(this).has('.fa-facebook').on('click', { category: 'Social Networks', label: 'Facebook' }, sendGAEvent);
+  $(this).has('.fa-instagram').on('click', { category: 'Social Networks', label: 'Instagram' }, sendGAEvent);
+  $(this).has('.fa-vimeo').on('click', { category: 'Social Networks', label: 'Vimeo' }, sendGAEvent);
+  $(this).has('.fa-youtube').on('click', { category: 'Social Networks', label: 'Youtube' }, sendGAEvent);
 
   /** Professional **/
-  $(this).has('.fa-twitter').on('click', { label: 'Twitter' }, sendGAEvent);
-  $(this).has('.fa-linkedin').on('click', { label: 'LinkedIn' }, sendGAEvent);
-  $(this).has('.fa-google-plus').on('click', { label: 'Google+' }, sendGAEvent);
-  $(this).has('.fa-about-me').on('click', { label: 'About.me' }, sendGAEvent);
-  $(this).has('.fa-klout').on('click', { label: 'Klout' }, sendGAEvent);
+  $(this).has('.fa-twitter').on('click', { category: 'Social Networks', label: 'Twitter' }, sendGAEvent);
+  $(this).has('.fa-linkedin').on('click', { category: 'Social Networks', label: 'LinkedIn' }, sendGAEvent);
+  $(this).has('.fa-google-plus').on('click', { category: 'Social Networks', label: 'Google+' }, sendGAEvent);
+  $(this).has('.fa-about-me').on('click', { category: 'Social Networks', label: 'About.me' }, sendGAEvent);
+  $(this).has('.fa-klout').on('click', { category: 'Social Networks', label: 'Klout' }, sendGAEvent);
+});
+
+$('#personal-website-links a.btn').each(function() {
+  $(this).has('.fa-globe').on('click', { category: 'Personal Website', label: 'Home' }, sendGAEvent);
+  $(this).has('.fa-rocket').on('click', { category: 'Personal Website', label: 'Projets' }, sendGAEvent);
+  $(this).has('.fa-rss-square').on('click', { category: 'Personal Website', label: 'Blog' }, sendGAEvent);
+  $(this).has('.fa-user').on('click', { category: 'Personal Website', label: 'CV' }, sendGAEvent);
 });
