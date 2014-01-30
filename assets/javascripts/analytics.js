846375d862c50de9ae88bc1648bd5ef651b5d53a
Frederic Duperier <prestataire.frederic.duperier@ca-titres.fr>
2014-01-30 13:55:04 +0100

    deleting closure in analytics.js

--- assets/javascripts/analytics.js
+++ assets/javascripts/analytics.js
@@ -1,26 +1,23 @@
-(function() {
-  "use strict";
+"use strict";
 
-  /* Google Analytics events */
-  var ga = ga || [];
+/* Google Analytics events */
+var ga = ga || [];
 
-  var sendGAEvent = function(e) {
-    ga('send', 'event', 'Social Networks', 'click', e.data.label);
-  };
+var sendGAEvent = function(e) {
+  ga('send', 'event', 'Social Networks', 'click', e.data.label);
+};
 
-  $('a.btn').each(function(index) {
-    /** Personal **/
-    $(this).has('.fa-facebook').on('click', { label: "Facebook" }, sendGAEvent);
-    $(this).has('.fa-instagram').on('click', { label: "Instagram" }, sendGAEvent);
-    $(this).has('.fa-vimeo').on('click', { label: "Vimeo" }, sendGAEvent);
-    $(this).has('.fa-youtube').on('click', { label: "Youtube" }, sendGAEvent);
+$('a.btn').each(function(index) {
+  /** Personal **/
+  $(this).has('.fa-facebook').on('click', { label: "Facebook" }, sendGAEvent);
+  $(this).has('.fa-instagram').on('click', { label: "Instagram" }, sendGAEvent);
+  $(this).has('.fa-vimeo').on('click', { label: "Vimeo" }, sendGAEvent);
+  $(this).has('.fa-youtube').on('click', { label: "Youtube" }, sendGAEvent);
 
-    /** Professional **/
-    $(this).has('.fa-twitter').on('click', { label: "Twitter" }, sendGAEvent);
-    $(this).has('.fa-linkedin').on('click', { label: "LinkedIn" }, sendGAEvent);
-    $(this).has('.fa-google-plus').on('click', { label: "Google+" }, sendGAEvent);
-    $(this).has('.fa-about-me').on('click', { label: "About.me" }, sendGAEvent);
-    $(this).has('.fa-klout').on('click', { label: "Klout" }, sendGAEvent);
-  });
-
-})();
+  /** Professional **/
+  $(this).has('.fa-twitter').on('click', { label: "Twitter" }, sendGAEvent);
+  $(this).has('.fa-linkedin').on('click', { label: "LinkedIn" }, sendGAEvent);
+  $(this).has('.fa-google-plus').on('click', { label: "Google+" }, sendGAEvent);
+  $(this).has('.fa-about-me').on('click', { label: "About.me" }, sendGAEvent);
+  $(this).has('.fa-klout').on('click', { label: "Klout" }, sendGAEvent);
+});
