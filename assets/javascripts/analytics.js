9ff5b8ae970e66e5f51758a5b02934bedb4f25c4
Frederic Duperier <prestataire.frederic.duperier@ca-titres.fr>
2014-01-30 11:53:33 +0100

    Deleting not-minified bootstrap files & cleaning Google Analytics events javascript

--- assets/javascripts/analytics.js
+++ assets/javascripts/analytics.js
@@ -1,35 +1,36 @@
 /* Google Analytics events */
 $(document).ready(function() {
 
-  /** Personal **/
-  $(document).on('click', '.g1-social-icons .g1-social-icon--facebook', function() {
-    ga('send', 'event', 'Social Networks', 'click', 'Facebook');
-  });
-  $(document).on('click', '.g1-social-icons .g1-social-icon--instagram', function() {
-    ga('send', 'event', 'Social Networks', 'click', 'Instagram');
-  });
-  $(document).on('click', '.g1-social-icons .g1-social-icon--vimeo', function() {
-    ga('send', 'event', 'Social Networks', 'click', 'Vimeo');
-  });
-  $(document).on('click', '.g1-social-icons .g1-social-icon--youtube', function() {
-    ga('send', 'event', 'Social Networks', 'click', 'Youtube');
-  });
+  $(document)
+    /** Personal **/
+    .on('click', '.g1-social-icons .g1-social-icon--facebook', function() {
+      ga('send', 'event', 'Social Networks', 'click', 'Facebook');
+    })
+    .on('click', '.g1-social-icons .g1-social-icon--instagram', function() {
+      ga('send', 'event', 'Social Networks', 'click', 'Instagram');
+    })
+    .on('click', '.g1-social-icons .g1-social-icon--vimeo', function() {
+      ga('send', 'event', 'Social Networks', 'click', 'Vimeo');
+    })
+    .on('click', '.g1-social-icons .g1-social-icon--youtube', function() {
+      ga('send', 'event', 'Social Networks', 'click', 'Youtube');
+    })
 
-  /** Professional **/
-  $(document).on('click', '.g1-social-icons .g1-social-icon--twitter', function() {
-    ga('send', 'event', 'Social Networks', 'click', 'Twitter');
-  });
-  $(document).on('click', '.g1-social-icons .g1-social-icon--linkedin', function() {
-    ga('send', 'event', 'Social Networks', 'click', 'LinkedIn');
-  });
-  $(document).on('click', '.g1-social-icons .g1-social-icon--googleplus', function() {
-    ga('send', 'event', 'Social Networks', 'click', 'Google+');
-  });
-  $(document).on('click', '.g1-social-icons .g1-social-icon--aboutme', function() {
-    ga('send', 'event', 'Social Networks', 'click', 'About.me');
-  });
-  $(document).on('click', '.g1-social-icons .g1-social-icon--klout', function() {
-    ga('send', 'event', 'Social Networks', 'click', 'Klout');
-  });
-
+    /** Professional **/
+    .on('click', '.g1-social-icons .g1-social-icon--twitter', function() {
+      ga('send', 'event', 'Social Networks', 'click', 'Twitter');
+    })
+    .on('click', '.g1-social-icons .g1-social-icon--linkedin', function() {
+      ga('send', 'event', 'Social Networks', 'click', 'LinkedIn');
+    })
+    .on('click', '.g1-social-icons .g1-social-icon--googleplus', function() {
+      ga('send', 'event', 'Social Networks', 'click', 'Google+');
+    })
+    .on('click', '.g1-social-icons .g1-social-icon--aboutme', function() {
+      ga('send', 'event', 'Social Networks', 'click', 'About.me');
+    })
+    .on('click', '.g1-social-icons .g1-social-icon--klout', function() {
+      ga('send', 'event', 'Social Networks', 'click', 'Klout');
+    })
+  ;
 });
