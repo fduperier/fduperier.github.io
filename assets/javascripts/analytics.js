e4dd341fb3ee8f26b1580bfae7f88cfd11bc0b40
Frederic Duperier <prestataire.frederic.duperier@ca-titres.fr>
2014-01-30 14:46:58 +0100

    Replacing " with '

--- assets/javascripts/analytics.js
+++ assets/javascripts/analytics.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 /* Google Analytics events */
 var ga = ga || [];
@@ -7,17 +7,17 @@ var sendGAEvent = function(e) {
   ga('send', 'event', 'Social Networks', 'click', e.data.label);
 };
 
-$('a.btn').each(function(index) {
+$('a.btn').each(function() {
   /** Personal **/
-  $(this).has('.fa-facebook').on('click', { label: "Facebook" }, sendGAEvent);
-  $(this).has('.fa-instagram').on('click', { label: "Instagram" }, sendGAEvent);
-  $(this).has('.fa-vimeo').on('click', { label: "Vimeo" }, sendGAEvent);
-  $(this).has('.fa-youtube').on('click', { label: "Youtube" }, sendGAEvent);
+  $(this).has('.fa-facebook').on('click', { label: 'Facebook' }, sendGAEvent);
+  $(this).has('.fa-instagram').on('click', { label: 'Instagram' }, sendGAEvent);
+  $(this).has('.fa-vimeo').on('click', { label: 'Vimeo' }, sendGAEvent);
+  $(this).has('.fa-youtube').on('click', { label: 'Youtube' }, sendGAEvent);
 
   /** Professional **/
-  $(this).has('.fa-twitter').on('click', { label: "Twitter" }, sendGAEvent);
-  $(this).has('.fa-linkedin').on('click', { label: "LinkedIn" }, sendGAEvent);
-  $(this).has('.fa-google-plus').on('click', { label: "Google+" }, sendGAEvent);
-  $(this).has('.fa-about-me').on('click', { label: "About.me" }, sendGAEvent);
-  $(this).has('.fa-klout').on('click', { label: "Klout" }, sendGAEvent);
+  $(this).has('.fa-twitter').on('click', { label: 'Twitter' }, sendGAEvent);
+  $(this).has('.fa-linkedin').on('click', { label: 'LinkedIn' }, sendGAEvent);
+  $(this).has('.fa-google-plus').on('click', { label: 'Google+' }, sendGAEvent);
+  $(this).has('.fa-about-me').on('click', { label: 'About.me' }, sendGAEvent);
+  $(this).has('.fa-klout').on('click', { label: 'Klout' }, sendGAEvent);
 });
